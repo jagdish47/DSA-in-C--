408af1aecf077741ce13d30cf91901cf0fc320bf
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-07-27 20:47:40 +0530

    Factorial Using Recursion.

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,1 +1,24 @@
-sdf
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int factorialRecursion(int n)
+{
+    //   Base case or (Base Condition)
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    return n * factorialRecursion(n - 1);
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int ans = factorialRecursion(n);
+    cout << ans << endl;
+}
