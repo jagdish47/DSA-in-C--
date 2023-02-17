5bbd29146dee4f017048f9aed122dda1026072ec
jagdishkumawat <jagdishkumawat81@gmail.com>
2023-02-17 09:24:44 +0530

    find duplicate

--- 2D-Array/homework-3.cpp
+++ 2D-Array/homework-3.cpp
@@ -3,6 +3,32 @@
 #include <limits.h>
 using namespace std;
 
+vector<int> duplicate(vector<int> arr, int n)
+{
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = arr[i] % n;
+        arr[index] += n;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((arr[i] / n) >= 2)
+        {
+            ans.push_back(i);
+        }
+    }
+
+    if (ans.size() == 0)
+    {
+        return {-1}; // this is way to return vector
+    }
+
+    return ans;
+}
+
 int main()
 {
 
@@ -32,33 +58,42 @@ int main()
 
     // APPROACH - 2 -------------------------------------------------------------
 
-    vector<int> arr = {1, 2, 3, 4, 5, 4, 3, 2, 10, 20};
+    // vector<int> arr = {1, 2, 3, 4, 5, 4, 3, 2, 10, 20};
 
-    int max = INT_MIN;
+    // int max = INT_MIN;
 
-    for (int i = 0; i < arr.size(); i++)
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+
+    //     if (arr[i] > max)
+    //     {
+    //         max = arr[i];
+    //     }
+    // }
+
+    // vector<int> vec(max, 0);
+
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     vec[arr[i]]++;
+    // }
+
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     if (vec[i] == 1)
+    //     {
+    //         cout << arr[i] << " ";
+    //     }
+    // }
+
+    // APPROACH-3=================================================================
+
+    vector<int> arr = {1, 2, 3, 1, 2};
+
+    vector<int> ve = duplicate(arr, arr.size());
+
+    for (int i = 0; i < ve.size(); i++)
     {
-
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
+        cout << ve[i] << " ";
     }
-
-    vector<int> vec(max, 0);
-
-    for (int i = 0; i < arr.size(); i++)
-    {
-        vec[arr[i]]++;
-    }
-
-    for (int i = 0; i < vec.size(); i++)
-    {
-        if (vec[i] == 1)
-        {
-            cout << arr[i] << " ";
-        }
-    }
-
-    return 0;
 }
