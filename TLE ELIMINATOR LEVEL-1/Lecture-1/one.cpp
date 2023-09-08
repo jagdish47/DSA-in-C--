7ad627d0a91ef63c7947beff9120242f05afea2b
jagdish47 <jagdishkumawat81@gmail.com>
2023-09-08 21:01:59 +0530

    Completed C++ Basics

---
+++ TLE ELIMINATOR LEVEL-1/Lecture-1/one.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // int a, b, c;
+    // cin >> a >> b >> c;
+
+    // If your using string you can not use space when you use space it will consider as new string
+    // Hello how are
+    // a -> Hello
+    // b -> how
+    // c -> you
+    string a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
+
+/**
+ * Operator
+ * Datatype
+ * Input
+ * Output
+ * if-else -> statements
+ * Type Conversion
+ *
+ */
