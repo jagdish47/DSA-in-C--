1c97e1d0ec6d502a0d7ac4cec70fe5d7ddb7bfb9
jagdish47 <jagdishkumawat81@gmail.com>
2023-09-14 09:58:28 +0530

    Fibonacci Series

---
+++ Recursion Revisions/fibonacci.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+
+    if (n == 1 || n == 1)
+    {
+        return 0;
+    }
+
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+
+    int n;
+    cout << "enter n : ";
+    cin >> n;
+
+    cout << "Ans : " << fib(n) << endl;
+
+    return 0;
+}
